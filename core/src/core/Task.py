1e864663f27261702126ef4b05ccb29cc98ef824
Alexis Magnier <aaleex3984@gmail.com>
2026-05-12 10:01:40 +0200

    move test into test/ package

--- core/src/core/Task.py
+++ core/src/core/Task.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Any
+
 from .types import *
 from .Policy import Query
 
@@ -10,6 +12,8 @@ class Task:
     id: TaskID = INVALID_TASK_ID
     name: str = ""
 
+    props: dict[str, Any] = field(default_factory=dict)
+
     query: Query = None 
  
     @staticmethod
@@ -17,6 +21,7 @@ class Task:
         return Task(
             id=data.get("id"),
             name=data.get("name", ""),
+            props=data.get("props", dict()),
 
             query=Query.From_dict(query_data)
             if (query_data := data.get("query", None)) is not None else None
